2b717dd0f60935a92a421115c62542cf6cd31ce4
2859521944@qq.com <2859521944@qq.com>
2026-07-28 13:29:27 +0800

    chore: clean use-system imports

--- frontend/src/shared/hooks/use-system.ts
+++ frontend/src/shared/hooks/use-system.ts
@@ -1,5 +1,4 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { toast } from "sonner";
 
 import { api } from "@/shared/api/client";
 import { useInvalidate } from "@/shared/hooks/use-invalidate";
